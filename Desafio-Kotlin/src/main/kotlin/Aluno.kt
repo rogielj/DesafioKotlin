72777e17a5e9f8e810d259468c66b858d4a64a2f
Rogiel <rogiel.anjos@gmail.com>
2021-09-01 19:50:00 -0300

    Parte A

---
+++ Desafio-Kotlin/src/main/kotlin/Aluno.kt
@@ -0,0 +1,24 @@
+class Aluno
+
+    (
+        var nome: String,
+        var sobrenome: String,
+        val codAluno: Int
+    ) {
+
+        override fun equals(other: Any?): Boolean {
+            return (other is Aluno && other.codAluno == this.codAluno)
+        }
+
+        init {
+
+            if (codAluno < 0) {
+                println("*** Codigo do Aluno não pode ser um Numero negativo ***\n")
+            }
+        }
+
+//    fun verificarAlunos (): Int {
+//        return codAluno
+//    }
+
+}
---
+++ Desafio-Kotlin/src/main/kotlin/Run.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("-------Verificando se os código são iguais------")
+    val aluno1 = Aluno("Matias", "K", 1234)
+    val aluno2 = Aluno("João", "Almeida", 1234)
+    val aluno3 = Aluno("Maria", "Antonieta", 7816)
+
+//    println("Com Equals")
+//    println(aluno1 == aluno2)
+//    println(aluno1 == aluno3)
+//    println()
+//    println(aluno2 == aluno1)
+//    println(aluno2 == aluno3)
+//    println()
+//    println(aluno3 == aluno1)
+//    println(aluno3 == aluno2)
+//    println()
+
+}
